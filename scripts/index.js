eac788e443e46e5a46e943676a795affa3559654
Leta Keane <letakeane+turing@gmail.com>
2017-06-26 22:42:07 -0600

    Add styling

--- scripts/index.js
+++ scripts/index.js
@@ -1,16 +1,21 @@
-
-$('#links-section').prepend('strings or links')
-
 const folderGenerator = (name) => {
   return(
     `
     <article class='folder'>
-      <img src='http://icons.iconarchive.com/icons/dtafalonso/yosemite-flat/512/Folder-icon.png'>
-      <h3>${name}</h3>
+      <img src='./images/folder.svg' class='folder-icon' alt='folder icon'>
+      <h2>${name}</h2>
     </article>`
   )
 }
 
-const thingToEnter = folderGenerator('an example title')
+const recipes = folderGenerator('recipes')
+const norwegianCathedrals = folderGenerator('norwegian cathedrals')
+const bestGrassSpecies = folderGenerator('best grass species')
+const weedkiller = folderGenerator('weedkiller')
+const pencilCups = folderGenerator('pencil cups')
 
-$('#links-section').prepend(thingToEnter)
+$('#links-section').prepend(pencilCups)
+$('#links-section').prepend(weedkiller)
+$('#links-section').prepend(bestGrassSpecies)
+$('#links-section').prepend(norwegianCathedrals)
+$('#links-section').prepend(recipes)
