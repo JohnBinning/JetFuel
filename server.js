731b178d6834de322df7ad5460a1b009a26763ae
Leta Keane <letakeane+turing@gmail.com>
2017-06-28 15:23:56 -0600

    Create API endpoints to GET and POST links

--- server.js
+++ server.js
@@ -32,46 +32,86 @@ app.get('/api/v1/folders', (request, response) => {
         })
       }
     })
-    .catch((error) => {
-      response.status(500).json({ error })
-    })
-})
-
-app.get('/api/v1/folders/:id', (request, response) => {
-  database('folders').where('id', request.params.id).select()
-  .then((folders) => {
-    if (folders.length) {
-      response.status(200).json(folders)
-    } else {
-      response.status(404).json({
-        error: `No folders found with id of ${request.params.id}`
-      })
-    }
-  })
   .catch((error) => {
     response.status(500).json({ error })
   })
 })
 
-app.post('/api/folders', (request, response) => {
-  console.log(' req.body', request.body);
-  const { name } = request.body
+app.get('/api/v1/folders/:id', (request, response) => {
+  database('folders').where('id', request.params.id).select()
+    .then((folders) => {
+      if (folders.length) {
+        response.status(200).json(folders)
+      } else {
+        response.status(404).json({
+          error: `No folders found with id of ${request.params.id}`
+        })
+      }
+    })
+  .catch((error) => {
+    response.status(500).json({ error })
+  })
+})
 
-  if(!name) {
+app.get('api/v1/folders/:id/links', (request, response) => {
+  database('links').where('folder_id', request.params.id).select()
+    .then((links) => {
+      if (links.length) {
+        response.status(200).json(links)
+      } else {
+        response.status(200).json({
+          message: `No links found in folder with id of ${request.params.id}`
+        })
+      }
+    })
+  .catch((error) => {
+    response.status(500).json({ error })
+  })
+})
+
+app.post('/api/v1/folders', (request, response) => {
+  const { folder } = request.body
+
+  if (!folder.name) {
     return response.status(422).send({
       error: 'No folder name provided'
     })
   }
 
-  database('folders').insert(folders, 'id')
-    .then((folders) => {
-      response.status(201).json({ id: folders[0] })
+  database('folders').insert(folder, 'id')
+    .then((folder) => {
+      response.status(201).json({ id: folder[0] })
     })
+  .catch((error) => {
+    response.status(500).json({ error })
+  })
+})
+
+app.post('/api/v1/links', (request, response) => {
+  const { link } = request.body
+
+  if (!link.shortened_url) {
+    return response.status(422).send({
+      error: 'An error occurred while generating the shortened url; please try again'
+    })
+  }
+
+  for (let requiredParameter of ['name', 'url', 'folder_id']) {
+      if (!link[requiredParameter]) {
+        return response.status(422).json({
+          error: `Expected format: { name: <String>, url: <String>, folder_id: <Integer> }. You are missing a ${requiredParameter} property.`
+        })
+      }
+    }
+
+    database('links').insert(link, 'id') //Inserting the link, returning the generated id of that paper
+      .then((linkId) => {
+        response.status(201).json({ id: linkId[0]})
+      })
     .catch((error) => {
       response.status(500).json({ error })
     })
 })
-
 
 
 
