536497a7d6a90b727ff0463464efb29d541bcdfe
Leta Keane <letakeane+turing@gmail.com>
2017-06-28 18:43:53 -0600

    Connect up client with server

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,3 +1,6 @@
+let foldersArray;
+let matchingFolder;
+
 const folderHtmlGenerator = (name) => {
   return(
     `
@@ -9,9 +12,20 @@ const folderHtmlGenerator = (name) => {
   )
 }
 
-const displayFolders = () => {
-  folderArray.forEach( folder => {
-    $('#folders-section').prepend(folder)
+const getFolders = () => {
+  fetch('/api/v1/folders')
+    .then((resp) => resp.json())
+    .then((folders) => {
+      foldersArray = folders;
+      displayFolders(folders);
+    })
+    .catch((error) => console.log('Problem retreiving folders: ', error))
+}
+
+const displayFolders = (folderArray) => {
+  folderArray.forEach((folder) => {
+    console.log("Displaying ", folder);
+    $('#folders-section').prepend(folderHtmlGenerator(folder.name))
   })
 }
 
@@ -19,26 +33,53 @@ const clearInputs = () => {
   $('input').val('')
 }
 
-$('.submit-btn').on('click', (e) => {
-  e.preventDefault()
-  let folderVal = $('.input-folder').val()
+const postFolder = (folderNameVal) => {
+  console.log("POSTING FOLDER!!!!!!!!!!");
 
-  folderPush(folderHtmlGenerator(folderVal))
-  $('#folders-section').html('')
-  displayFolders()
-  clearInputs()
-})
+  const header = { "Content-Type": "application/json" };
+  const body = { "name": `${folderNameVal}` };
 
-const recipes = folderHtmlGenerator('recipes')
-const norwegianCathedrals = folderHtmlGenerator('norwegian cathedrals')
-const bestGrassSpecies = folderHtmlGenerator('best grass species')
-const weedkiller = folderHtmlGenerator('weedkiller')
-const pencilCups = folderHtmlGenerator('pencil cups')
-const folderArray = [recipes, norwegianCathedrals, bestGrassSpecies, weedkiller, pencilCups]
-
-//need to rename this function badly
-const folderPush = (folder) => {
-  folderArray.push(folder)
+  return fetch('/api/v1/folders', {method: "POST", headers: header, body: JSON.stringify(body)})
+    .then(resp => resp.json())
+    .then(id => {
+      return id
+    })
+  .catch(error => console.log('Error retreiving folders: ', error))
 }
 
-displayFolders()
+const postLink = (linkNameVal, linkUrlVal, matchingFolder) => {
+  console.log('linkNameVal: ', linkNameVal);
+  console.log('linkUrlVal: ', linkUrlVal);
+  console.log('matchingFolder: ', matchingFolder);
+
+  const header = { "Content-Type": "application/json" };
+  const body = { "name": `${linkNameVal}`, "url": `${linkUrlVal}`, "folder_id": `${matchingFolder}` };
+
+  fetch('/api/v1/links', {method: "POST", headers: header, body: JSON.stringify(body)});
+}
+
+$('.submit-btn').on('click', (e) => {
+  e.preventDefault();
+  let linkUrlVal = $('.input-url').val();
+  let folderNameVal = $('.input-folder').val();
+  let linkNameVal = $('.input-name').val();
+
+  let matchingFolder = foldersArray.find((folder) => {
+    return folder.name === folderNameVal
+  })
+console.log("********matchingFolder: ", matchingFolder);
+
+  let folderToPass;
+  if (matchingFolder === undefined) {
+    folderToPass = parseInt(postFolder(folderNameVal));
+  } else {
+    folderToPass = matchingFolder.id;
+  }
+  postLink(linkNameVal, linkUrlVal, folderToPass);
+
+  clearInputs();
+  $('#folders-section').html('');
+  getFolders();
+})
+
+getFolders()
