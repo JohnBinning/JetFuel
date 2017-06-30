fdc9148f82ebd1274da38860f09b6c1a09137a4b
John Binning <john.binning17@gmail.com>
2017-06-30 07:47:22 -0600

    add server tests

--- db/migrations/20170630011612_initial_test.js
+++ db/migrations/20170630011612_initial_test.js
@@ -1,8 +1,16 @@
 
 exports.up = function(knex, Promise) {
-
+  const firstPromise = new Promise((resolve, reject) => {
+    resolve()
+    //don't throw warning
+  });
+  return firstPromise
 };
 
 exports.down = function(knex, Promise) {
-  
+  const firstPromise = new Promise((resolve, reject) => {
+    resolve()
+    //  also don't throw warning
+  });
+  return firstPromise
 };
