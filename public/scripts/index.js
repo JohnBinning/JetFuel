d5533509af960074e842b32a074570aae525f8b5
Leta Keane <letakeane+turing@gmail.com>
2017-06-30 09:38:30 -0600

    Add incredibly ugly url validation to form

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -26,6 +26,9 @@ const linkHtmlGenerator = (linkObject, newUrl) => {
   )
 }
 
+// const urlRegex = '_^(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)(?:\.(?:[a-z\x{00a1}-\x{ffff}0-9]+-?)*[a-z\x{00a1}-\x{ffff}0-9]+)*(?:\.(?:[a-z\x{00a1}-\x{ffff}]{2,})))(?::\d{2,5})?(?:/[^\s]*)?$_iuS'
+
+
 const displayLinks = (linksArray) => {
   linksArray.forEach( link => {
       const newUrl = `${link.shortened_url}`
@@ -115,31 +118,79 @@ const postLink = (linkNameVal, linkUrlVal, matchingFolder) => {
   fetch('/api/v1/links', {method: "POST", headers: header, body: JSON.stringify(body)});
 }
 
+const validateUrl = (urlInput) => {
+  let cleanUrl = getHostname(urlInput);
+
+  return cleanUrl;
+}
+
+const getHostname = (urlInput) => {
+  let nakedUrl = '';
+  if (urlInput.includes('http://')) {
+    let hostname;
+
+    url.indexOf("://") > -1 ? hostname = url.split('/')[2] : hostname = url.split('/')[0]
+
+    //find & remove port number
+    hostname = hostname.split(':')[0];
+    //find & remove "?"
+    hostname = hostname.split('?')[0];
+
+    nakedUrl = hostname;
+    let verifiedUrl = verifyUrl(nakedUrl);
+    return verifiedUrl;
+  } else {
+    nakedUrl = urlInput;
+    let verifiedUrl = verifyUrl(nakedUrl);
+    return verifiedUrl;
+  }
+  return verifiedUrl;
+}
+
+const verifyUrl = (nakedUrl) => {
+  if (!nakedUrl.includes('.')) {
+    $('form').prepend(`<p id='error-alert'>Please enter a valid URL</p>`)
+
+    $('.input-url').on('focus', () => {
+      $('#error-alert').html('')
+    })
+  } else {
+    return nakedUrl;
+  }
+}
+
+
 $('.submit-btn').on('click', (e) => {
   e.preventDefault();
   let linkUrlVal = $('.input-url').val();
   let folderNameVal = $('.input-folder').val();
   let linkNameVal = $('.input-name').val();
 
-  let matchingFolder = foldersArray.find((folder) => {
-    return folder.name === folderNameVal
-  })
+  const urlToStore = validateUrl(linkUrlVal);
+  console.log(urlToStore);
 
-  let folderToPass = 'default folder to pass';
+  if (urlToStore) {
+    let matchingFolder = foldersArray.find((folder) => {
+      return folder.name === folderNameVal
+    })
 
-  if (matchingFolder == undefined) {
-    postFolder(folderNameVal)
-      .then( folder_id => {
-        getFolders()
-        postLink(linkNameVal, linkUrlVal, folder_id.id)
-      })
-  } else {
-    postLink(linkNameVal, linkUrlVal, matchingFolder.id);
+    let folderToPass = 'default folder to pass';
+
+    if (matchingFolder == undefined) {
+      postFolder(folderNameVal)
+        .then( folder_id => {
+          getFolders()
+          postLink(linkNameVal, urlToStore, folder_id.id)
+        })
+    } else {
+      postLink(linkNameVal, urlToStore, matchingFolder.id);
+    }
+
+    clearInputs();
+    $('#folders-section').html('');
+    getFolders();
   }
 
-  clearInputs();
-  $('#folders-section').html('');
-  getFolders();
 })
 
 getFolders()
