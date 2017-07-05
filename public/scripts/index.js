82ca8b834892616c00457e178be5dcd6c937360b
Leta Keane <letakeane+turing@gmail.com>
2017-07-05 14:17:09 -0600

    Lint index.js

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,96 @@
 let foldersArray = [];
 let displayedLinkArray = [];
-let matchingFolder;
 let mostToLeast = true;
 const domain = 'steelbirdfood.herokuapp.com';
 
 // form section
+const clearInputs = (linkUrlVal, folderNameVal, linkNameVal) => {
+  if (linkUrlVal !== '' || folderNameVal !== '' || linkNameVal !== '') {
+    $('input').val('');
+  }
+};
+
+const findMatchingFolder = (foldersArray, folderNameVal) => {
+  return foldersArray.find((folder) => {
+    return folder.name === folderNameVal;
+  });
+};
+
+const verifyTld = (hostname) => {
+  if (!hostname.includes('.')) {
+    $('form').prepend(`<article class='error-alert'><p class='error-alert-text'>Please enter a valid URL</p></article>`);
+
+    $('.input-url').on('focus', () => {
+      $('.error-alert').empty();
+    });
+  } else {
+    return hostname;
+  }
+};
+
+const getHostname = (urlInput) => {
+  let hostname;
+  let verifiedUrl;
+  if (urlInput.includes('http://') || urlInput.includes('https://')) {
+
+    hostname = urlInput.split('://')[1];
+
+    verifiedUrl = verifyTld(hostname);
+    return verifiedUrl;
+  } else {
+    hostname = urlInput;
+    verifiedUrl = verifyTld(hostname);
+    return verifiedUrl;
+  }
+  return verifiedUrl;
+};
+
+const removeProtocol = (urlInput) => {
+  let cleanUrl = getHostname(urlInput);
+
+  return cleanUrl;
+};
+
+const incompleteFormErrorMessage = () => {
+  $('form').prepend(`<article class='error-alert'><p class='error-alert-text'>Please fill out all input fields</p></article>`);
+
+  $('.error-alert').on('focus', () => {
+    $('.error-alert').empty();
+  });
+};
+
+const successMessage = (name) => {
+  $('form').append(`<article class='success-message'><p class='success-message-text'>SUCCESS! Your shortened link is in the ${name} folder below!</p></article>`);
+
+  $('form').on('focusout', () => {
+    $('.success-message').empty();
+  });
+};
+
+const postFolder = (folderNameVal) => {
+
+  const header = { "Content-Type": "application/json" };
+  const body = { "name": `${folderNameVal}` };
+
+  return fetch('/api/v1/folders', {method: "POST", headers: header, body: JSON.stringify(body)})
+    .then(resp => resp.json())
+    .then(id => {
+
+      return id;
+    })
+  .catch(error => console.log('Error retreiving folders: ', error));
+};
+
+const getFolders = () => {
+  fetch('/api/v1/folders')
+    .then((resp) => resp.json())
+    .then((folders) => {
+      foldersArray = folders;
+      $('#folders-section').empty();
+      displayFolders(folders);
+    })
+    .catch((error) => console.log('Problem retreiving folders: ', error));
+};
 
 $('.submit-btn').on('click', (e) => {
   e.preventDefault();
@@ -23,7 +109,6 @@ $('.submit-btn').on('click', (e) => {
 
   if (urlToStore) {
     let matchingFolder = findMatchingFolder(foldersArray, folderNameVal);
-    let folderToPass = 'default folder to pass';
 
     if (matchingFolder == undefined && urlToStore.includes('.') && linkUrlVal !== '' && folderNameVal !== '' && linkNameVal !== '') {
       postFolder(folderNameVal)
@@ -33,7 +118,7 @@ $('.submit-btn').on('click', (e) => {
           successMessage(folderNameVal);
           clearInputs(linkUrlVal, folderNameVal, linkNameVal);
         })
-        .catch((error) => console.log('Error posting folder: ', error))
+        .catch((error) => console.log('Error posting folder: ', error));
     } else {
       getFolders();
       postLink(linkNameVal, urlToStore, matchingFolder.id);
@@ -42,69 +127,8 @@ $('.submit-btn').on('click', (e) => {
     }
 
   }
-})
+});
 
-const clearInputs = (linkUrlVal, folderNameVal, linkNameVal) => {
-  if (linkUrlVal !== '' || folderNameVal !== '' || linkNameVal !== '') {
-    $('input').val('')
-  }
-}
-
-const findMatchingFolder = (foldersArray, folderNameVal) => {
-  return foldersArray.find((folder) => {
-    return folder.name === folderNameVal
-  })
-}
-
-const removeProtocol = (urlInput) => {
-  let cleanUrl = getHostname(urlInput);
-
-  return cleanUrl;
-}
-
-const getHostname = (urlInput) => {
-  if (urlInput.includes('http://') || urlInput.includes('https://')) {
-    let hostname;
-
-    hostname = urlInput.split('://')[1];
-
-    let verifiedUrl = verifyTld(hostname);
-    return verifiedUrl;
-  } else {
-    hostname = urlInput;
-    let verifiedUrl = verifyTld(hostname);
-    return verifiedUrl;
-  }
-  return verifiedUrl;
-}
-
-const verifyTld = (hostname) => {
-  if (!hostname.includes('.')) {
-    $('form').prepend(`<article class='error-alert'><p class='error-alert-text'>Please enter a valid URL</p></article>`)
-
-    $('.input-url').on('focus', () => {
-      $('.error-alert').empty();
-    })
-  } else {
-    return hostname;
-  }
-}
-
-const incompleteFormErrorMessage = () => {
-  $('form').prepend(`<article class='error-alert'><p class='error-alert-text'>Please fill out all input fields</p></article>`)
-
-  $('.error-alert').on('focus', () => {
-    $('.error-alert').empty();
-  })
-}
-
-const successMessage = (name) => {
-  $('form').append(`<article class='success-message'><p class='success-message-text'>SUCCESS! Your shortened link is in the ${name} folder below!</p></article>`)
-
-  $('form').on('focusout', () => {
-    $('.success-message').empty();
-  })
-}
 
 // folder section
 
@@ -116,42 +140,15 @@ const folderHtmlGenerator = (name) => {
       <h2 class='folder-name'>${name}</h2>
     </article>
     `
-  )
-}
+  );
+};
 
-const displayFolders = (folderArray) => {
-  folderArray.forEach((folder) => {
-    $('#folders-section').prepend(folderHtmlGenerator(folder.name))
-    clickFolders()
-  })
-}
+// folder section server requests
 
-  // folder section server requests
-
-const postFolder = (folderNameVal) => {
-
-  const header = { "Content-Type": "application/json" };
-  const body = { "name": `${folderNameVal}` };
-
-  return fetch('/api/v1/folders', {method: "POST", headers: header, body: JSON.stringify(body)})
-    .then(resp => resp.json())
-    .then(id => {
-
-      return id
-    })
-  .catch(error => console.log('Error retreiving folders: ', error))
-}
-
-const getFolders = () => {
-  fetch('/api/v1/folders')
-    .then((resp) => resp.json())
-    .then((folders) => {
-      foldersArray = folders;
-      $('#folders-section').empty();
-      displayFolders(folders);
-    })
-    .catch((error) => console.log('Problem retreiving folders: ', error))
-}
+const displayLinks = (linksArray) => {
+  $('#folders-section').empty();
+  return mostToLeast ? appendLinks(linksArray) : prependLinks(linksArray);
+};
 
 const clickFolders = () => {
   $('.folder-icon').on('click', (e) => {
@@ -159,24 +156,24 @@ const clickFolders = () => {
     $('h3').addClass('hidden');
     $('.sort-by-visits').removeClass('hidden');
 
-    let folderName = e.target.closest('.folder').id
+    let folderName = e.target.closest('.folder').id;
 
     if (!foldersArray.length) {
-      return
+      return;
     }
 
     let newMatchingFolder = foldersArray.find( folder => {
-      return folder.name === folderName
-    })
+      return folder.name === folderName;
+    });
     fetch(`/api/v1/folders/${newMatchingFolder.id}/links`)
       .then((response) => response.json())
       .then((linksArray) => {
         displayedLinkArray = linksArray;
-        displayLinks(linksArray)
+        displayLinks(linksArray);
       })
-    .catch((error) => console.log('error retrieving links: ', error))
-  })
-}
+    .catch((error) => console.log('error retrieving links: ', error));
+  });
+};
 
 // link section
 
@@ -191,45 +188,40 @@ const linkHtmlGenerator = (linkObject, newUrl) => {
       </h2>
     </article>
     `
-  )
-}
+  );
+};
 
 const appendLinks = (linksArray) => {
   linksArray.forEach( link => {
-    const newUrl = `${link.shortened_url}`
-    $('#folders-section').append(linkHtmlGenerator(link, newUrl))
-    clickLinks(link)
+    const newUrl = `${link.shortened_url}`;
+    $('#folders-section').append(linkHtmlGenerator(link, newUrl));
+    clickLinks(link);
   });
-}
+};
 
 const prependLinks = (linksArray) => {
   linksArray.forEach( link => {
-    const newUrl = `${link.shortened_url}`
-    $('#folders-section').prepend(linkHtmlGenerator(link, newUrl))
-    clickLinks(link)
+    const newUrl = `${link.shortened_url}`;
+    $('#folders-section').prepend(linkHtmlGenerator(link, newUrl));
+    clickLinks(link);
   });
-}
+};
 
-const displayLinks = (linksArray) => {
-  $('#folders-section').empty();
-  mostToLeast ? appendLinks(linksArray) : prependLinks(linksArray)
-}
 
 const clickLinks = () => {
   $('a').on('click', (e) => {
     const id = e.target.closest('.link').id;
-    const url = e.target.closest('.link').classList[1];
     incrementLinkVisits(id);
-  })
-}
+  });
+};
 
-  // link section server requests
+// link section server requests
 
 const incrementLinkVisits = (linkId) => {
   fetch(`/api/v1/links/click/${linkId}`)
     .then((response) => response.json())
-  .catch((error) => console.log('Error incrementing link visits: ', error))
-}
+  .catch((error) => console.log('Error incrementing link visits: ', error));
+};
 
 const postLink = (linkNameVal, linkUrlVal, matchingFolder) => {
 
@@ -237,43 +229,44 @@ const postLink = (linkNameVal, linkUrlVal, matchingFolder) => {
   const body = { "name": `${linkNameVal}`, "url": `${linkUrlVal}`, "folder_id": `${matchingFolder}`, "visits": 0 };
 
   fetch('/api/v1/links', {method: "POST", headers: header, body: JSON.stringify(body)});
-}
-
-const redirectLink = (url) => {
-  fetch(`/api/${url}`)
-    .then((response) => response.json())
-  .catch((error) => console.log('Error redirecting: ', error))
-}
+};
 
 // controls
+
+const displayFolders = (folderArray) => {
+  folderArray.forEach((folder) => {
+    $('#folders-section').prepend(folderHtmlGenerator(folder.name));
+    clickFolders();
+  });
+};
 
 $('.search').on('keyup', (e) => {
   if(foldersArray && foldersArray.length) {
     $('#folders-section').empty();
     let filtered = foldersArray.filter(folder => {
-      return folder.name.toLowerCase().includes(e.target.value.toLowerCase())
-    })
-    displayFolders(filtered)
+      return folder.name.toLowerCase().includes(e.target.value.toLowerCase());
+    });
+    displayFolders(filtered);
   }
-})
+});
 
 $('.sort-by-visits').on('click', () => {
   sortLinks();
   mostToLeast = !mostToLeast;
-})
+});
 
 const sortLinks = () => {
   const sortedMostLeastLinks = displayedLinkArray.sort((a, b) => {
-    return a.visits < b.visits
-  })
+    return a.visits < b.visits;
+  });
 
   displayLinks(sortedMostLeastLinks);
-}
+};
 
 $('.title').on('click', () => {
-  location.reload()
-})
+  location.reload();
+});
 
 // page load
 
-getFolders()
+getFolders();
