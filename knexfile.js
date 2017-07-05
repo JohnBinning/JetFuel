c802c62696547da2829464c095b7feb4a59f3b04
Leta Keane <letakeane+turing@gmail.com>
2017-07-05 13:45:21 -0600

    Create validation repo

--- knexfile.js
+++ knexfile.js
@@ -25,19 +25,15 @@ module.exports = {
     }
   },
   staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
   },
 
   production: {
